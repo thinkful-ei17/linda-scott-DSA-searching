ebe8f72bf7036c911a9943efb89cc4d72193fbec
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-07 09:50:22 -0800

    added html file with input and search buttons

--- linearSearch.js
+++ linearSearch.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
 Given the following dataset, find out how many tries did it take to search for a particular item in the dataset. If the item is not in the dataset, provide a message and indicate how many searches did it take to do so.
 
@@ -6,7 +7,7 @@ Given the following dataset, find out how many tries did it take to search for a
 
 //const arr = [89,30,25,32,72,70,51,42,25,24,53,55,78,50,13,40,48,32,26,2,14,33,45,72,56,44,21,88,27,68,15,62,93,98,73,28,16,46,87,28,65,38,67,16,85,63,23,69,64,91,9,70,81,27,97,82,6,88,3,7,46,13,11,64,76,31,26,38,28,13,17,69,90,1,6,7,64,43,9,73,80,98,46,27,22,87,49,83,6,39,42,51,54,84,34,53,78,40,14,5];
 const arr = [89,30,25,32,72,70,51,42,25,24,53,55,78,50,13,40,48,32,26,2,14,33,45,72,56];
-const value = 89;
+// const value = 89;
 let count = 0;
 
 function linearSearch(arr, value) {
@@ -18,9 +19,9 @@ function linearSearch(arr, value) {
       return value;
     }
   }
-    console.log('Item was not found');
-    console.log('Number of tries was', count);
-    return -1 //convention to use -1, indicate not found to leverage for other functions that utilize linearSearch
+  console.log('Item was not found');
+  console.log('Number of tries was', count);
+  return -1;//convention to use -1, indicate not found to leverage for other functions that utilize linearSearch
 }
 
 
@@ -42,7 +43,7 @@ function binarySearch(arr, value, start=0, end=arr.length-1) {
   if (start > end) return -1;
 
   if (value === item) {
-    console.log('array', sortedArray);
+    // console.log('array', sortedArray);
     console.log('count equals', count);
     //console.log(sortedArray[index]);
     return index;
@@ -58,4 +59,14 @@ function binarySearch(arr, value, start=0, end=arr.length-1) {
 }
 
 
-console.log(binarySearch(arr, value));
+// console.log(binarySearch(arr, value));
+
+$(){
+
+  $('#linear').submit(function(e){
+    e.preventDefault();
+    const value = $('input').val();
+    
+    $('.results').html('linearSearch(arr, value)');
+  });
+}
